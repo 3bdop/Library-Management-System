85c727a73a1abcbe8e9b9cf68b8ea169662ca7b6
Bara AlOmari <60300383@udst.edu.qa>
2025-03-26 15:19:57 +0300

    Update loans table

--- db-tables/loans.sql
+++ db-tables/loans.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 25, 2025 at 12:40 PM
+-- Generation Time: Mar 26, 2025 at 01:18 PM
 -- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- PHP Version: 8.0.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -28,19 +28,56 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `loans` (
+  `loan_id` int(11) NOT NULL,
   `isbn` varchar(32) NOT NULL,
-  `customer` varchar(255) NOT NULL,
+  `member_id` int(11) NOT NULL,
   `loan_date` date NOT NULL,
   `due_date` date NOT NULL,
-  `fine_per_day` int(11) NOT NULL
+  `fine_per_day` decimal(10,2) NOT NULL DEFAULT 5.00,
+  `returned` tinyint(1) DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `loans`
 --
 
-INSERT INTO `loans` (`isbn`, `customer`, `loan_date`, `due_date`, `fine_per_day`) VALUES
-('9781617295522', 'ali', '2025-03-25', '2025-03-26', 5);
+INSERT INTO `loans` (`loan_id`, `isbn`, `member_id`, `loan_date`, `due_date`, `fine_per_day`, `returned`) VALUES
+(11, '9780596009205', 8, '2025-03-26', '2025-03-28', 5.00, 0),
+(12, '9780131103627', 8, '2025-03-26', '2025-04-05', 5.00, 0),
+(13, '9781491950357', 9, '2025-03-26', '2025-03-27', 5.00, 0);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `loans`
+--
+ALTER TABLE `loans`
+  ADD PRIMARY KEY (`loan_id`),
+  ADD KEY `member_id` (`member_id`),
+  ADD KEY `isbn` (`isbn`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `loans`
+--
+ALTER TABLE `loans`
+  MODIFY `loan_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `loans`
+--
+ALTER TABLE `loans`
+  ADD CONSTRAINT `loans_ibfk_1` FOREIGN KEY (`member_id`) REFERENCES `members` (`member_id`),
+  ADD CONSTRAINT `loans_ibfk_2` FOREIGN KEY (`isbn`) REFERENCES `books` (`isbn`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
