52ddc0484655d422088bd10102275af7185b371a
Bara AlOmari <60300383@udst.edu.qa>
2025-03-27 13:08:17 +0300

    Update members table

--- db-tables/members.sql
+++ db-tables/members.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 26, 2025 at 01:22 PM
+-- Generation Time: Mar 27, 2025 at 11:03 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
@@ -40,7 +40,11 @@ CREATE TABLE `members` (
 
 INSERT INTO `members` (`member_id`, `name`, `phone`, `registration_date`) VALUES
 (8, 'Mohammed Ali', 77069912, '2025-03-26'),
-(9, 'Khalid Ahmed', 55422092, '2025-03-26');
+(9, 'Khalid Ahmed', 55422092, '2025-03-26'),
+(10, 'Faisal Abdullah', 33123499, '2025-03-26'),
+(11, 'Hussam Fetyan', 55033822, '2025-03-27'),
+(12, 'Ali Khalil', 33452200, '2025-03-27'),
+(16, 'Robert Ford', 77089943, '2025-03-27');
 
 --
 -- Indexes for dumped tables
@@ -61,7 +65,7 @@ ALTER TABLE `members`
 -- AUTO_INCREMENT for table `members`
 --
 ALTER TABLE `members`
-  MODIFY `member_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `member_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
