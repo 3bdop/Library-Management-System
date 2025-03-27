ac8a32d05e9103f61ada2f3bed3833c33a6100a0
Bara AlOmari <60300383@udst.edu.qa>
2025-03-27 13:06:45 +0300

    Update loans table

--- db-tables/loans.sql
+++ db-tables/loans.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 26, 2025 at 01:18 PM
+-- Generation Time: Mar 27, 2025 at 11:03 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
@@ -34,17 +34,27 @@ CREATE TABLE `loans` (
   `loan_date` date NOT NULL,
   `due_date` date NOT NULL,
   `fine_per_day` decimal(10,2) NOT NULL DEFAULT 5.00,
-  `returned` tinyint(1) DEFAULT 0
+  `returned` tinyint(1) DEFAULT 0,
+  `return_date` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `loans`
 --
 
-INSERT INTO `loans` (`loan_id`, `isbn`, `member_id`, `loan_date`, `due_date`, `fine_per_day`, `returned`) VALUES
-(11, '9780596009205', 8, '2025-03-26', '2025-03-28', 5.00, 0),
-(12, '9780131103627', 8, '2025-03-26', '2025-04-05', 5.00, 0),
-(13, '9781491950357', 9, '2025-03-26', '2025-03-27', 5.00, 0);
+INSERT INTO `loans` (`loan_id`, `isbn`, `member_id`, `loan_date`, `due_date`, `fine_per_day`, `returned`, `return_date`) VALUES
+(11, '9780596009205', 8, '2025-03-26', '2025-03-28', 5.00, 1, '2025-03-26'),
+(12, '9780131103627', 8, '2025-03-26', '2025-04-05', 5.00, 0, NULL),
+(13, '9781491950357', 9, '2025-03-26', '2025-03-24', 5.00, 1, '2025-03-26'),
+(14, '9781492078005', 10, '2025-03-26', '2025-04-09', 5.00, 1, '2025-03-26'),
+(15, '9780596009205', 8, '2025-03-26', '2025-03-26', 5.00, 1, '2025-03-26'),
+(16, '9781617295522', 11, '2025-03-27', '2025-03-27', 5.00, 1, '2025-03-27'),
+(18, '9780132350884', 11, '2025-03-05', '2025-03-10', 5.00, 1, '2025-03-27'),
+(19, '9781617295522', 12, '2025-03-18', '2025-03-25', 5.00, 1, '2025-03-27'),
+(20, '9780132350884', 12, '2025-03-20', '2025-03-25', 5.00, 0, NULL),
+(21, '9781491950357', 10, '2025-03-27', '2025-04-20', 5.00, 1, '2025-03-27'),
+(22, '9780596009205', 16, '2025-03-05', '2025-03-20', 5.00, 0, NULL),
+(23, '9781492078005', 11, '2025-03-27', '2025-03-29', 5.00, 0, NULL);
 
 --
 -- Indexes for dumped tables
@@ -66,7 +76,7 @@ ALTER TABLE `loans`
 -- AUTO_INCREMENT for table `loans`
 --
 ALTER TABLE `loans`
-  MODIFY `loan_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `loan_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
 --
 -- Constraints for dumped tables
