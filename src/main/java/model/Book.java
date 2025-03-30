e402cf2162e82f5cab4923e4de7a1a636e9b4bf4
3bdop <abdbinm55@gmail.com>
2025-03-30 23:02:04 +0300

    update book class

--- src/main/java/model/Book.java
+++ src/main/java/model/Book.java
@@ -1,67 +1,49 @@
 package main.java.model;
 
 public class Book {
-    private int bookId;
-    private String title;
-    private String author;
-    private String isbn;
-    private String category;
-    private String status;
+    private final int bookId;
+    private final String isbn;
+    private final String title;
+    private final String author;
+    private final String category;
+    private final String publishedYear;
+    private final String status;
 
-    public Book(String isbn, String category, String status, String author, String title, int bookId) {
-        this.isbn = isbn;
-        this.category = category;
-        this.status = status;
-        this.author = author;
-        this.title = title;
+    public Book(int bookId, String isbn, String title, String author, String category, String publishedYear, String status) {
         this.bookId = bookId;
+        this.isbn = isbn;
+        this.title = title;
+        this.author = author;
+        this.category = category;
+        this.publishedYear = publishedYear;
+        this.status = status;
     }
 
     public int getBookId() {
         return bookId;
     }
 
-    public void setBookId(int bookId) {
-        this.bookId = bookId;
+    public String getIsbn() {
+        return isbn;
     }
 
     public String getTitle() {
         return title;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
     public String getAuthor() {
         return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public String getIsbn() {
-        return isbn;
-    }
-
-    public void setIsbn(String isbn) {
-        this.isbn = isbn;
     }
 
     public String getCategory() {
         return category;
     }
 
-    public void setCategory(String category) {
-        this.category = category;
+    public String getPublishedYear() {
+        return publishedYear;
     }
 
     public String getStatus() {
         return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
     }
 }
